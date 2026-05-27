b31425de77bab704b73576dcc03f9cb7e307af85
김수빈 <sbkim099@naver.com>
2026-05-28 03:44:49 +0900

    전자결재 휴가신청서 결재 상신 기능 완료

--- src/main/java/com/study/app/domains/approval/ApprovalDAO.java
+++ src/main/java/com/study/app/domains/approval/ApprovalDAO.java
@@ -10,5 +10,19 @@ public class ApprovalDAO {
 	@Autowired
 	private SqlSessionTemplate mybatis;
 	
+	public int insertDraftOfVacation(DraftDocumentsDTO dto) {
+		return mybatis.insert("Approval.insertDraftOfVacation", dto);
+	}
 	
+	public int insertApprovalLines(ApprovalLinesDTO dto) {
+		return mybatis.insert("Approval.insertApprovalLines", dto);
+	}
+	
+	public int insertReferrers(ApprovalCcDTO dto) {
+		return mybatis.insert("Approval.insertReferrers", dto);
+	}
+	
+	public int insertVacationDetail(VacationDTO dto) {
+		return mybatis.insert("Approval.insertVacationDetail", dto);
+	}
 }
