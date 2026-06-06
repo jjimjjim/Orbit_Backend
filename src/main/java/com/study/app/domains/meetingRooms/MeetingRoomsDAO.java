2abc7e502919b06b5ffbda14afd5f2c6734af886
김수빈 <sbkim099@naver.com>
2026-06-07 05:07:44 +0900

    회의실 관리 페이지 회의실 추가 기능 구현 완료

--- src/main/java/com/study/app/domains/meetingRooms/MeetingRoomsDAO.java
+++ src/main/java/com/study/app/domains/meetingRooms/MeetingRoomsDAO.java
@@ -1,4 +1,22 @@
 package com.study.app.domains.meetingRooms;
 
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class MeetingRoomsDAO {
+	
+	@Autowired
+	private SqlSessionTemplate mybatis;
+	
+	public List<MeetingRoomsDTO> getAllRooms(){
+		return mybatis.selectList("MeetingRooms.getAllRooms");
+	}
+	
+	public void addMeetingRoom(MeetingRoomsDTO dto) {
+		mybatis.insert("MeetingRooms.addMeetingRoom", dto);
+	}
 }
--- src/main/java/com/study/app/domains/meetingRooms/MeetingRoomsService.java
+++ src/main/java/com/study/app/domains/meetingRooms/MeetingRoomsService.java
@@ -1,7 +1,39 @@
 package com.study.app.domains.meetingRooms;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.study.app.domains.file.FileService;
 
 @Service
 public class MeetingRoomsService {
+	
+	@Autowired
+	private MeetingRoomsDAO dao;
+	@Autowired
+	private FileService fileServ;
+	
+	public List<MeetingRoomsDTO> getAllRooms(){
+		return dao.getAllRooms();
+	}
+	
+	@Transactional
+	public void addMeetingRoom(MeetingRoomsDTO dto, MultipartFile file) {
+		if (file != null && !file.isEmpty()) {
+			try {
+				Map<String, String> fileInfo = fileServ.upload(file);
+
+				dto.setOriname(fileInfo.get("oriname"));
+				dto.setSysname(fileInfo.get("sysname"));
+			}catch(Exception e) {
+				throw new RuntimeException("파일 업로드 실패", e);
+			}
+		}
+		dao.addMeetingRoom(dto);
+	}
 }
